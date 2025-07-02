d39c376ddf5bcdc2fa58cca3932b9e2b79a589d7
anshuman-coder <anshurockingboy23@gmail.com>
2025-07-03 03:04:22 +0530

    adding the prefix and creating register http requests

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const port = process.env.PORT;
+  app.setGlobalPrefix('/api/v1');
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   await app.listen(port || 4000);
 }
